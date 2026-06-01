e6adf5f34dc9047b990667a61a1fc5f75271b0e4
oakshiai <oakshiai@gmail.com>
2026-06-01 14:41:41 +0300

    Created a wishlist of features.

--- src/components/Terminal/Grok/Grok.jsx
+++ src/components/Terminal/Grok/Grok.jsx
@@ -1,24 +1,26 @@
 import {useTerminal} from '../TerminalContext.js';
 
-const Line = ({from, to}) => {
+const Line = ({from, to, color}) => {
   const width = to.x - from.x;
   const height = to.y - from.y;
 
   console.log(width, height);
+  // TODO: Implement
+};
+
+const Text = ({from, color}) => {
+  // TODO: Implement
 };
 
 const Grok = () => {
   const terminal = useTerminal();
-  console.log(terminal);
+  // TODO: Design and implement a drawing system where you can work on a canvas of characters, placing them on a 2D grid, if there's a conflict for a cell - the more recent element overwrites that cell.
 
   return (
     <>
-      <Line from={{x: 1, y: 2}} to={{x: 10, y:2}} />
-      {Array.from(Array(110).keys()).map((_, index) => {
-        return (
-          <div key={index}>{index}Test<span style={{color: 'red'}}>red</span>black</div>
-        );
-      })}
+      <Line from={{x: 0, y: 2}} to={{x: terminal.width, y: 2}} color="red" />
+      <Line from={{x: 0, y: 3}} to={{x: 0, y: terminal.height}} color="black" />
+      <Text from={{x: 10, y: 10}} color="orange">Something that goes on that line and breaks if needed</Text>
     </>
   );
 };
