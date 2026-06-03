50307697fb1d6acd90ebb480c73f6717ff65f2a3
oakshiai <oakshiai@gmail.com>
2026-06-03 13:52:04 +0300

    Removed cursor updating for this primitive, moved that upstream.

--- src/components/Terminal/Grok/drawCharacter.js
+++ src/components/Terminal/Grok/drawCharacter.js
@@ -26,9 +26,6 @@ const drawCharacter = (surface, x, y, character, color) => {
   } else {
     drawInCanvas(surface, x, y, character, color);
   }
-
-  surface.cursor = {x: x + 1, y};
-  surface.color = color;
 };
 
 export {drawCharacter};
