1aec5a282202ad422c9d4b95d2a123ea13ee2650
oakshiai <oakshiai@gmail.com>
2026-06-03 16:49:08 +0300

    Fixed drawing in both directions.

--- src/components/Terminal/Grok/drawLine.js
+++ src/components/Terminal/Grok/drawLine.js
@@ -1,10 +1,14 @@
 import {drawCharacter} from './drawCharacter.js';
 
 const drawLine = (surface, end, {color} = {}) => {
-  const deltaX = end.x ? Math.abs(end.x - surface.cursor.x) : 0;
-  const deltaY = end.y ? Math.abs(end.y - surface.cursor.y) : 0;
+  const deltaX = end.x !== undefined ? Math.abs(end.x - surface.cursor.x) : 0;
+  const deltaY = end.y !== undefined ? Math.abs(end.y - surface.cursor.y) : 0;
   const character = deltaX > 0 ? '─' : '│';
-  const start = deltaX > 0 ? surface.cursor.x : surface.cursor.y;
+  const start = (
+    deltaX > 0 ?
+    Math.min(surface.cursor.x, end.x) :
+    Math.min(surface.cursor.y, end.y)
+  );
   const length = deltaX > 0 ? deltaX : deltaY;
 
   for (let i = start; i <= (start + length); i += 1) {
--- src/components/Terminal/Grok/drawLine.test.js
+++ src/components/Terminal/Grok/drawLine.test.js
@@ -5,7 +5,7 @@ import {createCanvas, renderCanvasToRows} from './canvas.js';
 import {drawLine} from './drawLine.js';
 import {moveTo} from './moveTo.js';
 
-test('drawLine draws a horizontal line from the cursor', () => {
+test('drawLine draws a horizontal line from the cursor to the right', () => {
   const canvas = createCanvas(6, 3);
 
   moveTo(canvas, {x: 1, y: 1});
@@ -19,11 +19,41 @@ test('drawLine draws a horizontal line from the cursor', () => {
   assert.deepEqual(canvas.cursor, {x: 5, y: 1});
 });
 
-test('drawLine draws a vertical line from the cursor', () => {
+test('drawLine draws a horizontal line from the cursor to the left', () => {
+  const canvas = createCanvas(6, 3);
+
+  moveTo(canvas, {x: 4, y: 1});
+  drawLine(canvas, {x: 1});
+
+  assert.deepEqual(renderCanvasToRows(canvas), [
+    '      ',
+    ' ──── ',
+    '      ',
+  ]);
+  assert.deepEqual(canvas.cursor, {x: 5, y: 1});
+});
+
+test('drawLine draws a vertical line from the cursor down', () => {
   const canvas = createCanvas(4, 5);
 
   moveTo(canvas, {x: 2, y: 1});
   drawLine(canvas, {y: 3});
+
+  assert.deepEqual(renderCanvasToRows(canvas), [
+    '    ',
+    '  │ ',
+    '  │ ',
+    '  │ ',
+    '    ',
+  ]);
+  assert.deepEqual(canvas.cursor, {x: 2, y: 4});
+});
+
+test('drawLine draws a vertical line from the cursor up', () => {
+  const canvas = createCanvas(4, 5);
+
+  moveTo(canvas, {x: 2, y: 3});
+  drawLine(canvas, {y: 1});
 
   assert.deepEqual(renderCanvasToRows(canvas), [
     '    ',
