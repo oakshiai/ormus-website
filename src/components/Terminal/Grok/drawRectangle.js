1051e66a168e3463678730cec73bffdb00f81105
oakshiai <oakshiai@gmail.com>
2026-06-03 19:39:33 +0300

    Refactor drawRectangle to use moveTo for cursor positioning and update parameter names for clarity.

--- src/components/Terminal/Grok/drawRectangle.js
+++ src/components/Terminal/Grok/drawRectangle.js
@@ -1,21 +1,26 @@
+import {moveTo} from './moveTo.js';
 import {drawCharacter} from './drawCharacter.js';
 import {drawLine} from './drawLine.js';
 
-const drawRectangle = (canvas, origin, size, color) => {
+const drawRectangle = (surface, origin, size, color) => {
   const left = origin.x;
   const top = origin.y;
   const right = origin.x + size.width - 1;
   const bottom = origin.y + size.height - 1;
 
-  drawLine(canvas, {x: left, y: top}, {x: right, y: top}, color);
-  drawLine(canvas, {x: left, y: bottom}, {x: right, y: bottom}, color);
-  drawLine(canvas, {x: left, y: top}, {x: left, y: bottom}, color);
-  drawLine(canvas, {x: right, y: top}, {x: right, y: bottom}, color);
+  moveTo(surface, {x: left, y: top});
+  drawLine(surface, {x: right}, {color});
+  moveTo(surface, {deltaX: -1});
+  drawLine(surface, {y: bottom}, {color});
+  moveTo(surface, {deltaY: -1});
+  drawLine(surface, {x: left}, {color});
+  moveTo(surface, {deltaX: 1});
+  drawLine(surface, {y: top}, {color});
 
-  drawCharacter(canvas, left, top, '╭', color);
-  drawCharacter(canvas, right, top, '╮', color);
-  drawCharacter(canvas, left, bottom, '╰', color);
-  drawCharacter(canvas, right, bottom, '╯', color);
+  drawCharacter(surface, left, top, '╭', color);
+  drawCharacter(surface, right, top, '╮', color);
+  drawCharacter(surface, left, bottom, '╰', color);
+  drawCharacter(surface, right, bottom, '╯', color);
 };
 
 export {drawRectangle};
